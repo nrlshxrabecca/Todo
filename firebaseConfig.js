f388c76047f10337abfeabfa4868fbb47f49377d
unknown <nrlshxrabecca@example.com>
2023-05-17 00:48:46 +0800

    first push

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+// Optionally import the services that you want to use
+// import {...} from "firebase/auth";
+
+
---
+++ modules/Header.js
@@ -0,0 +1,22 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
+import { firebase } from "../firebaseConfig";
+import { ArrowLeftOnRectangleIcon } from "react-native-heroicons/outline";
+import { useNavigation } from "@react-navigation/native";
+
+const Header = (props) => {
+  const navigation = useNavigation();
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (firebase.auth().currentUser) {
+     setLoggedIn(true);
+    }
+  }, []);
+
+  return (
+    <View className="items-center text-center flex-row justify-center mx-10">
+      <Text className="text-white text-3xl font-bold flex-1">
+        {props.title}
+      </Text>
+     
